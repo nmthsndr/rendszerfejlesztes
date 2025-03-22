0034b1c0894bd4bc71a33eceb21e646255640413
Olivér Bauer <GA2NQ6@student.uni-pannon.hu>
2025-03-22 14:24:12 +0100

    User

---
+++ HotelGuru/HotelGuru.DataContext/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuru.DataContext.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<Address> Address { get; set; }
+        public List<Role> Roles { get; set; }
+    }
+}
