af9bb5905a8fc6613242e8c0bc3db45a24fdf802
Kovács Dániel <kovacsdani042@gmail.com>
2025-03-22 14:40:10 +0100

    created Address class

---
+++ HotelGuru/HotelGuru.DataContext/Entities/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuru.DataContext.Entities
+{
+    public class Address
+    {
+        public int ZipCode { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+    }
+}
