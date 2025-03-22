48b551b1e1d2a12c3718d968bb385d21cb22cfe6
CsukOl <n7xq49@student.uni-pannon.hu>
2025-03-22 14:58:03 +0100

    Hozzáadtam a Room osztályt

---
+++ HotelGuru/HotelGuru.DataContext/Entities/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuru.DataContext.Entities
+{
+    public class Room
+    {
+        public int roomId { get; set; }
+        public int roomNumber { get; set; }
+        public string type { get; set; }
+        public int price { get; set; }
+        public bool avaible { get; set; }
+    }
+}
