6c3b13725fe3395cec457bdac4b06f14a625428b
nmthsndr <nmthsndr4@icloud.com>
2025-03-22 15:05:41 +0100

    #

--- HotelGuru/HotelGuru.DataContext/Entities/Room.cs
+++ HotelGuru/HotelGuru.DataContext/Entities/Room.cs
@@ -8,10 +8,10 @@ namespace HotelGuru.DataContext.Entities
 {
     public class Room
     {
-        public int roomId { get; set; }
-        public int roomNumber { get; set; }
-        public string type { get; set; }
-        public int price { get; set; }
-        public bool avaible { get; set; }
+        public int RoomId { get; set; }
+        public int RoomNumber { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+        public bool Avaible { get; set; }
     }
 }
