0660290f7b37cb4dba437127734e76d22cf69c0b
Olivér Bauer <GA2NQ6@student.uni-pannon.hu>
2025-03-22 14:36:58 +0100

    Hotel,Role

--- HotelGuru/HotelGuru.DataContext/obj/Debug/net8.0/HotelGuru.DataContext.AssemblyInfo.cs
+++ HotelGuru/HotelGuru.DataContext/obj/Debug/net8.0/HotelGuru.DataContext.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelGuru.DataContext")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd3ebb54061e037ddd37aa57e5c4e7f8c90bda1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07dd8c7b1f286adf753a30357336ea2a024e65ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelGuru.DataContext")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelGuru.DataContext")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
