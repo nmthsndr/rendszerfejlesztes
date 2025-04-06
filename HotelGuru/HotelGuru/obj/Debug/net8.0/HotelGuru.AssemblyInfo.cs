0d1aaaec3d759b7ae12782aeb41cdc8b8951df8b
Olivér Bauer <GA2NQ6@student.uni-pannon.hu>
2025-04-06 13:49:14 +0200

    UserController

--- HotelGuru/HotelGuru/obj/Debug/net8.0/HotelGuru.AssemblyInfo.cs
+++ HotelGuru/HotelGuru/obj/Debug/net8.0/HotelGuru.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelGuru")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50cf8f9aef7a101bac2c7ace2d10ccde243f092e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+339ede31071364efdef3026d91665c32badd92e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelGuru")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelGuru")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
