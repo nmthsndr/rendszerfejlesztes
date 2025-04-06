8364615d3b49628cbe8e50016ff6672c847271fb
Olivér Bauer <GA2NQ6@student.uni-pannon.hu>
2025-04-06 16:23:54 +0200

    Controllers

---
+++ HotelGuru/HotelGuru/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelGuru.Controllers
+{
+    public class AdminController
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class UserController : ControllerBase
+        { 
+        
+        }
+    }
+}
---
+++ HotelGuru/HotelGuru/Controllers/ReceptionistController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelGuru.Controllers
+{
+    public class ReceptionistController
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class UserController : ControllerBase
+        { 
+        
+        }
+    }
+}
