8c83cfc0ec5b26e70ff187feff4c8717ed34d398
Olivér Bauer <GA2NQ6@student.uni-pannon.hu>
2025-04-06 15:01:27 +0200

    USerDto edit

--- HotelGuru/HotelGuru.DataContext/Dtos/UserDto.cs
+++ HotelGuru/HotelGuru.DataContext/Dtos/UserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,56 @@ namespace HotelGuru.DataContext.Dtos
     public class UserDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public IList<RoleDto> Role { get; set; }
+        public IList<AddressDto> Addresses { get; set; }
+    }
+
+    public class UserRegisterDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        public IList<int> RoleIds { get; set; }
+    }
+
+    public class UserLoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class UserUpdateDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        public IList<int> RoleIds { get; set; }
     }
 }
